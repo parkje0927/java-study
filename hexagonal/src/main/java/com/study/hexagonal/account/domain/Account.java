926930bf5233a00ef1f2643deeaf642ff1790eb7
parkje0927 <parkje09270@gmail.com>
2023-03-17 23:26:57 +0900

    feat: Account 도메인 예제 코드
    - 도메인 엔티티를 중심으로 유스케이스 구현

--- hexagonal/src/main/java/com/study/hexagonal/account/domain/Account.java
+++ hexagonal/src/main/java/com/study/hexagonal/account/domain/Account.java
@@ -1,4 +1,57 @@
 package com.study.hexagonal.account.domain;
 
+import java.time.LocalDateTime;
+
 public class Account {
+
+    /**
+     * 계좌에 대한 모든 입금과 출금은 Activity 엔티티에 포착된다.
+     * 모든 activity 를 한꺼번에 메모리에 올리는 것은 현명한 방법이 아니다.
+     */
+
+    private AccountId id;
+    private Money baselineBalance;
+    private ActivityWindow activityWindow;
+
+    //getter, setter, constructor
+
+    public Money calculateBalance() {
+        return Money.add(
+                this.baselineBalance,
+                this.activityWindow.calculateBalance(this.id);
+        )
+    }
+
+    public boolean withdraw(Money money, AccountId targetAccountId) {
+        if (!mayWithdraw(money)) {
+            return false;
+        }
+
+        Activity withdrawal = new Activity(
+                this.id,
+                this.id,
+                targetAccountId,
+                LocalDateTime.now(),
+                money
+        );
+
+        this.activityWindow.addActivity(withdrawal);
+        return true;
+    }
+
+    private boolean mayWithdraw(Money money) {
+        return Money.add(this.calculateBalance(), money.negate()).isPositive();
+    }
+
+    public boolean deposit(Money money, AccountId sourceAccountId) {
+        Activity deposit = new Activity(
+                this.id,
+                sourceAccountId,
+                this.id,
+                LocalDateTime.now(),
+                money
+        );
+        this.activityWindow.addActivity(deposit);
+        return true;
+    }
 }
