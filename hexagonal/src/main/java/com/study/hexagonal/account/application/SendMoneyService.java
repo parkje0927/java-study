81bda5bc704bea58ed3e8ab27de5b5c2cd28597f
parkje0927 <parkje09270@gmail.com>
2023-03-17 23:31:30 +0900

    feat: 유스케이스 분리 예제 코드

--- hexagonal/src/main/java/com/study/hexagonal/account/application/SendMoneyService.java
+++ hexagonal/src/main/java/com/study/hexagonal/account/application/SendMoneyService.java
@@ -2,6 +2,17 @@ package com.study.hexagonal.account.application;
 
 import com.study.hexagonal.account.application.port.in.SendMoneyUseCase;
 import com.study.hexagonal.account.application.port.out.LoadAccountPort;
+import com.study.hexagonal.account.application.port.out.UpdateAccountStatePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
+@Transactional
 public class SendMoneyService implements SendMoneyUseCase {
+
+    private final LoadAccountPort loadAccountPort; //계좌를 불러오기 위해 아웃고잉 포트 인터페이스 호출
+    private final AccountLock accountLock;
+    private final UpdateAccountStatePort updateAccountStatePort; //DB 에 계좌 상태를 업데이트하기 위해 호출
+
+
 }
