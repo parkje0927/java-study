1b99ea1fb8e2f417e6c701619f90849b960d1d4a
parkje0927 <parkje09270@gmail.com>
2023-08-08 22:15:23 +0900

    refactor: 패키지명 변경

--- reactive/src/main/java/com/study/reactive/reactiveprogramming/function/TestC.java
+++ reactive/src/main/java/com/study/reactive/reactiveprogramming/function/TestC.java
@@ -1,4 +1,4 @@
-package com.study.reactive.chapter1.function;
+package com.study.reactive.reactiveprogramming.function;
 
 import lombok.extern.slf4j.Slf4j;
 
---
+++ reactive/src/main/java/com/study/reactive/reactiveprogramming/function/TestD.java
@@ -0,0 +1,43 @@
+package com.study.reactive.reactiveprogramming.function;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
+@Slf4j
+public class TestD {
+
+    public static void main(String[] args) {
+        log.info("함수 호출 관점 D start");
+        getResult(integer -> {
+            var nextValue = integer + 5;
+            assert nextValue == 5;
+        });
+        log.info("함수 호출 관점 D finish");
+    }
+
+    public static void getResult(Consumer<Integer> callback) {
+        var executorService = Executors.newSingleThreadExecutor();
+
+        try {
+            executorService.submit(() -> {
+                log.info("getResult start");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    log.error("error : {}", e);
+                }
+
+                var result = 0;
+                try {
+                    callback.accept(result);
+                } finally {
+                    log.info("getResult finish");
+                }
+            });
+        } finally {
+            executorService.shutdown();
+        }
+    }
+}
