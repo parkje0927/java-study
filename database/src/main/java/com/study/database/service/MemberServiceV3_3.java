3a09c0c7ad2cc08d9c91216f6eec0daeb809dc15
parkje0927 <parkje09270@gmail.com>
2023-10-22 23:00:18 +0900

    feat: 스프링 DB - 트랜잭션 AOP 활용

---
+++ database/src/main/java/com/study/database/service/MemberServiceV3_3.java
@@ -0,0 +1,45 @@
+package com.study.database.service;
+
+import com.study.database.domain.Member;
+import com.study.database.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - AOP
+ */
+@Slf4j
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        //비즈니스 로직
+        logic(fromId, toId, money);
+    }
+
+    private void logic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+
+        //오류 생성
+        validation(toMember);
+
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외가 발생했습니다.");
+        }
+    }
+}
