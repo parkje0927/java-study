270e4c97c365750c1c6bc3eea44ead1a39e3885e
parkje0927 <parkje09270@gmail.com>
2023-12-17 22:46:57 +0900

    test: 데이터 접근 예외 직접 생성(MyDuplicateKeyException) 및 테스트 코드 추가

---
+++ database/src/main/java/com/study/database/repository/exception/MyDuplicateKeyException.java
@@ -0,0 +1,19 @@
+package com.study.database.repository.exception;
+
+public class MyDuplicateKeyException extends RuntimeException {
+
+    public MyDuplicateKeyException() {
+    }
+
+    public MyDuplicateKeyException(String message) {
+        super(message);
+    }
+
+    public MyDuplicateKeyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyDuplicateKeyException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ database/src/test/java/com/study/database/exception/translator/ExTranslatorV1Test.java
@@ -0,0 +1,96 @@
+package com.study.database.exception.translator;
+
+import com.study.database.domain.Member;
+import com.study.database.repository.exception.MyDBException;
+import com.study.database.repository.exception.MyDuplicateKeyException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Random;
+
+import static com.study.database.connection.ConnectionConst.PASSWORD;
+import static com.study.database.connection.ConnectionConst.URL;
+import static com.study.database.connection.ConnectionConst.USERNAME;
+
+@Slf4j
+public class ExTranslatorV1Test {
+
+    Repository repository;
+    Service service;
+
+    @BeforeEach
+    void init() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new Repository(dataSource);
+        service = new Service(repository);
+    }
+
+    @Test
+    void duplicateKeySave() {
+        service.create("test");
+        service.create("test");
+    }
+
+    @Slf4j
+    @RequiredArgsConstructor
+    static class Service {
+        private final Repository repository;
+
+        public void create(String memberId) {
+            try {
+                repository.save(new Member(memberId, 0));
+                log.info("saveId = {}", memberId);
+            } catch (MyDuplicateKeyException e) {
+                log.info("키 중복!, 복구 시도");
+                String retryId = generateNewId(memberId);
+                log.info("retryId = {}", memberId);
+                repository.save(new Member(retryId, 0));
+            } catch (MyDBException e) {
+                log.info("데이터 접근 계층 예외", e);
+                throw e;
+            }
+        }
+
+        private String generateNewId(String memberId) {
+            return memberId + new Random().nextInt(10000);
+        }
+
+    }
+
+    @RequiredArgsConstructor
+    static class Repository {
+        private final DataSource dataSource;
+
+        public Member save(Member member) {
+            String sql = "insert into member(member_id, money) values(?, ?)";
+            Connection con = null;
+            PreparedStatement preparedStatement = null;
+
+            try {
+                con = dataSource.getConnection();
+                preparedStatement = con.prepareStatement(sql);
+                preparedStatement.setString(1, member.getMemberId());
+                preparedStatement.setInt(2, member.getMoney());
+                preparedStatement.executeUpdate();
+                return member;
+            } catch (SQLException e) {
+                //h2 db
+                if (e.getErrorCode() == 23505) {
+                    throw new MyDuplicateKeyException(e);
+                }
+                throw new MyDBException(e);
+            } finally {
+                JdbcUtils.closeStatement(preparedStatement);
+                JdbcUtils.closeConnection(con);
+            }
+        }
+    }
+}
