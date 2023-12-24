af7d30e746e736eb948ebfef7fd8cb8d7e20b366
parkje0927 <parkje09270@gmail.com>
2023-12-24 19:59:01 +0900

    feat: eureka 기본 설정 추가

--- msa/src/main/java/com/study/msa/MsaApplication.java
+++ msa/src/main/java/com/study/msa/MsaApplication.java
@@ -2,12 +2,14 @@ package com.study.msa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsaApplication.class, args);
+    }
 
 }
