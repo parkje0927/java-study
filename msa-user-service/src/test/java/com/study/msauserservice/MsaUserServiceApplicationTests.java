5c2cd6afde63fc9f3844b614411c3a4e913dda63
parkje0927 <parkje09270@gmail.com>
2023-12-25 23:08:43 +0900

    feat: msa user service 모듈 생성

---
+++ msa-user-service/src/test/java/com/study/msauserservice/MsaUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.msauserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
