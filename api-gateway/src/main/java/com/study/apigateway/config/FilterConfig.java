2ab285ba3bb8e8c737455a3a1d680c011d6024ea
parkje0927 <parkje09270@gmail.com>
2024-01-12 22:31:10 +0900

    feat: GatewayFilter 적용 시 Config 활용하는 방법 예시 추가

---
+++ api-gateway/src/main/java/com/study/apigateway/config/FilterConfig.java
@@ -0,0 +1,22 @@
+package com.study.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    /**
+     * 예시
+     */
+//    @Bean
+//    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(r -> r.path("/first-service/**")
+//                        .filters(f -> f.addRequestHeader("first-request", "first-request-header")
+//                                .addResponseHeader("first-response", "first-response-header"))
+//                        .uri("http://localhost:8081")
+//                )
+//                .build();
+//    }
+
+}
