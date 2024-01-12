f0d19daace379eb0d86b18f464e27cfcdd2f44f0
parkje0927 <parkje09270@gmail.com>
2024-01-12 22:31:32 +0900

    feat: GatewayFilter 적용 시 yml 파일에 설정 추가하는 방법 적용

--- eureka-first-service/src/main/java/com/study/firstservice/controller/FirstServiceController.java
+++ eureka-first-service/src/main/java/com/study/firstservice/controller/FirstServiceController.java
@@ -1,8 +1,11 @@
 package com.study.firstservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class FirstServiceController {
 
@@ -14,5 +17,11 @@ public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "first-service-welcome";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info("header = {}", header);
+        return header;
     }
 }
