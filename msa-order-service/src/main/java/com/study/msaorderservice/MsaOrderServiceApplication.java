dbb199edcc83226e30c433caf2159cf4e7284548
parkje0927 <parkje09270@gmail.com>
2024-01-30 21:59:38 +0900

    feat: order microservice 추가

---
+++ msa-order-service/src/main/java/com/study/msaorderservice/MsaOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.study.msaorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaOrderServiceApplication.class, args);
+    }
+
+}
