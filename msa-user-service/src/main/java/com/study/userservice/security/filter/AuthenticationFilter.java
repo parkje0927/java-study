2d5619e23d05794ed00faced135527d062afc5ca
parkje0927 <parkje09270@gmail.com>
2024-02-06 22:07:48 +0900

    feat: AuthenticationFilter 수정 및 SecurityConfig 에 추가

--- msa-user-service/src/main/java/com/study/userservice/security/filter/AuthenticationFilter.java
+++ msa-user-service/src/main/java/com/study/userservice/security/filter/AuthenticationFilter.java
@@ -1,12 +1,14 @@
 package com.study.userservice.security.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.userservice.user.dto.request.LoginRequest;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
@@ -14,9 +16,20 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    private final ObjectMapper objectMapper;
+
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws RuntimeException {
+        try {
+            LoginRequest loginRequest = objectMapper.readValue(request.getInputStream(), LoginRequest.class);
+            return getAuthenticationManager()
+                    .authenticate(new UsernamePasswordAuthenticationToken(
+                            loginRequest.email(), loginRequest.password()
+                    ));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
